925479f9382fc03e727599ba711582a5b9e7ad00
jayto581 <joseph@freeukgenealogy.org.uk>
2025-12-02 20:06:42 -0500

    test: baptism_record

---
+++ spec/integration/testing_baptism_records_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+require Rails.root.join('lib/freereg1_translator')
+
+describe 'Baptism SearchName population' do
+  let(:place) { create(:place, place_name: 'Guildford', chapman_code: 'ESS') }
+  let(:church) { create(:church, place: place) }
+  let(:register) { create(:register, church: church, register_type: 'Baptism') }
+  let(:file) { create(:freereg1_csv_file, :baptism_file, register: register) }
+  let(:entry) { create(:freereg1_csv_entry, :baptism_with_witnesses, freereg1_csv_file: file) }
+
+  it 'creates complete SearchRecord with all names' do
+    search_params = Freereg1Translator.translate(file, entry)
+    record = build(:search_record, :baptism_with_witnesses, freereg1_csv_entry: entry, place: place)
+    record.transform
+
+    # ap record.search_names
+    # ap record.search_names.count
+    # puts "Count: #{record.search_names.count}"
+    # puts "baptism date: #{entry.baptism_date}"  
+
+    # ap record.search_names.map(&:role)
+    # expect(record.search_names.count).to be >= 5
+    expect(record.search_names.map(&:role)).to include('ba', 'f', 'm', 'wt')
+  end
+end
