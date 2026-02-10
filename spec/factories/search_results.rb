17211b5d6a01f28e3c03118434a03a355152e839
jayto581 <joseph@freeukgenealogy.org.uk>
2026-02-09 19:27:36 -0500

    test: fix embedded model factory bug

--- spec/factories/search_results.rb
+++ spec/factories/search_results.rb
@@ -1,10 +1,13 @@
 FactoryBot.define do
   factory :search_result do
+    skip_create  # 
+    to_create { |_instance| }  # absolutely prevent persistence
+    # Prevent standalone persistence
+    initialize_with { new(attributes) }
+
     records        { {} }
     viewed_records { [] }
     ucf_records    { [] }
 
-    # Prevent standalone persistence
-    initialize_with { new(attributes) }
   end
 end
