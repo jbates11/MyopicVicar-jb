507c992464f5b95b08059fa7b4739b3922c7dfbc
jayto581 <joseph@freeukgenealogy.org.uk>
2026-03-07 13:32:33 -0500

    test: config database_cleaner support

--- spec/support/configuration/database_cleaner.rb
+++ spec/support/configuration/database_cleaner.rb
@@ -1,17 +1,24 @@
 RSpec.configure do |config|
-  # 1. Set Mongoid's default cleaning strategy to "delete documents".
-  # 2. Immediately wipe all collections in the test database.
+  # Strategy for Mongoid (MongoDB): Delete documents after each test
+  # This removes all test-created records from MongoDB collections
+  # Strategy for ActiveRecord (MySQL/Refinery): Transactional wrapping
+  # This wraps each test in a transaction and rolls back after completion,
+  # automatically removing test-created records while preserving pre-existing data.
+  
   config.before(:suite) do
     DatabaseCleaner[:mongoid].strategy = :deletion
     DatabaseCleaner[:mongoid].clean_with(:deletion)
+    
+    DatabaseCleaner[:active_record].strategy = :transaction
   end
 
   config.before(:each) do
     DatabaseCleaner[:mongoid].start
+    DatabaseCleaner[:active_record].start
   end
 
   config.after(:each) do
     DatabaseCleaner[:mongoid].clean
+    DatabaseCleaner[:active_record].clean
   end
-
 end
