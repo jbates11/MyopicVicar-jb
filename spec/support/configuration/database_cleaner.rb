09889c9b0a705e636fc5c97d4371c77ce26396a4
jayto581 <joseph@freeukgenealogy.org.uk>
2026-03-07 17:22:12 -0500

    test: reset database_cleaner for mongodb only

--- spec/support/configuration/database_cleaner.rb
+++ spec/support/configuration/database_cleaner.rb
@@ -1,24 +1,21 @@
 RSpec.configure do |config|
   # Strategy for Mongoid (MongoDB): Delete documents after each test
-  # This removes all test-created records from MongoDB collections
-  # Strategy for ActiveRecord (MySQL/Refinery): Transactional wrapping
-  # This wraps each test in a transaction and rolls back after completion,
-  # automatically removing test-created records while preserving pre-existing data.
+  # This removes all test-created records from MongoDB collections.
+  # 
+  # ActiveRecord (MySQL/Refinery) is handled by RSpec's use_transactional_fixtures = true,
+  # which wraps each test in a transaction and rolls back automatically,
+  # removing test-created records while preserving pre-existing data.
   
   config.before(:suite) do
     DatabaseCleaner[:mongoid].strategy = :deletion
     DatabaseCleaner[:mongoid].clean_with(:deletion)
-    
-    DatabaseCleaner[:active_record].strategy = :transaction
   end
 
   config.before(:each) do
     DatabaseCleaner[:mongoid].start
-    DatabaseCleaner[:active_record].start
   end
 
   config.after(:each) do
     DatabaseCleaner[:mongoid].clean
-    DatabaseCleaner[:active_record].clean
   end
 end
